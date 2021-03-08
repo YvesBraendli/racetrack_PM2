0020481d2e620d9dae5ad52705c7f64a484c1641
meierr02 <meierr02@students.zhaw.ch>
2021-03-08 17:44:22 +0100

    Added multiple methods

--- src/main/java/ch/zhaw/pm2/racetrack/Input.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Input.java
@@ -1,5 +1,46 @@
 package ch.zhaw.pm2.racetrack;
 
-public class Input {
+import java.io.File;
+import java.util.HashMap;
+import java.util.ArrayList;
 
+import org.beryx.textio.TextIO;
+
+
+
+public class Input {
+	private TextIO textIO;
+	private char[] possiblePlayerMoves = {'1','2','3','4','5','6','7','8','9','h','t','q'};
+	
+	
+	public Input(TextIO textIO) {
+		this.textIO = textIO;
+	}
+	
+	
+	public File requestTrack(File trackDirectory) {
+		File[] trackPathFiles = trackDirectory.listFiles();
+	
+		HashMap<String, File> files = new HashMap<String, File>();
+		for(File file: trackPathFiles) {
+			if(file.getName().endsWith(".txt")) {
+				files.put(file.getName(), file);
+			}
+		}
+		
+		return files.get(textIO.newStringInputReader()
+				.withNumberedPossibleValues(new ArrayList<String>(files.keySet()))
+				.read("Please select a track:"));	
+	}
+	
+	
+	public Config.StrategyType requestPlayerStrategy(char CarID) {
+		return textIO.newEnumInputReader(Config.StrategyType.class).read("Car " + CarID + ": Please select your strategy:");
+	}
+	
+//	public char requestPlayerMove() {
+//		return textIO.newCharInputReader().(possiblePlayerMoves).read("Please select a move");
+//	}
+	
+	
 }
--- src/main/java/ch/zhaw/pm2/racetrack/Output.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Output.java
@@ -1,5 +1,32 @@
 package ch.zhaw.pm2.racetrack;
 
-public class Output {
+import org.beryx.textio.TextTerminal;
 
+public class Output {
+	private TextTerminal<?> textTerminal;
+
+	public Output(TextTerminal<?> textTerminal) {
+		this.textTerminal = textTerminal;
+	}
+	
+	public void printTrack(String track) {
+		textTerminal.println(track);
+	}
+	
+	public void printDoNotMoveMessage() {
+		textTerminal.println("Nothing happened...");
+	}
+	
+	public void printWinner(char carID) {
+		textTerminal.println("Car " + carID + " won!");
+	}
+	
+	public void printWelcomeMessage() {
+		textTerminal.println("Welcome to Fischbein Racetrack 2021!");
+		
+	}
+	
+	public void printCurrentPlayer(char carID) {
+		textTerminal.println("Car " + carID + "'s turn");
+	}
 }
