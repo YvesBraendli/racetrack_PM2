46cbd8264e89acc11960a683bd7be0f1119d2b29
meierr02 <meierr02@students.zhaw.ch>
2021-03-14 14:40:29 +0100

    Refactoring

--- src/main/java/ch/zhaw/pm2/racetrack/Output.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Output.java
@@ -5,6 +5,7 @@ import org.beryx.textio.TextTerminal;
 /**
  * This class is used for all the outputs to the console which do not require
  * player input.
+ * 
  * @author Moser Nadine, Meier Robin, Brändli Yves
  *
  */
@@ -13,82 +14,87 @@ public class Output {
 
 	/**
 	 * The constructor for the Output class
+	 * 
 	 * @param textTerminal the terminal
 	 */
 	public Output(TextTerminal<?> textTerminal) {
 		this.textTerminal = textTerminal;
 	}
-	
+
 	/**
 	 * Prints the track
-	 * @param track 
+	 * 
+	 * @param track
 	 */
 	public void printTrack(String track) {
 		textTerminal.println(track);
 	}
-	
+
 	/**
 	 * Message for the do not move strategy
 	 */
 	public void printDoNotMoveMessage() {
 		textTerminal.println("Nothing happened...");
 	}
-	
+
 	/**
 	 * Prints the winner
+	 * 
 	 * @param carID the winners symbol
 	 */
 	public void printWinner(char carID) {
 		textTerminal.println("Car " + carID + " won!");
 	}
-	
+
 	/**
-	 * Prints the welcome message 
+	 * Prints the welcome message
 	 */
 	public void printWelcomeMessage() {
 		textTerminal.println("Welcome to Fischbein Racetrack 2021!");
-		
+
 	}
-	
+
 	/**
 	 * Prints a turn switch message
+	 * 
 	 * @param carID the symbol of the car whose turn it is
 	 */
 	public void printCurrentPlayer(char carID) {
 		textTerminal.println("Car " + carID + "'s turn");
 	}
-	
+
 	/**
 	 * Prints the Acceleration help page
 	 */
 	public void printAccelerationGrid() {
-		textTerminal.println("7 8 9	7=up-left 8=up 9=up-right\n"
-				+ "4 5 6	4=left 5=no acceleration 6=right\n"
+		textTerminal.println("7 8 9	7=up-left 8=up 9=up-right\n" + "4 5 6	4=left 5=no acceleration 6=right\n"
 				+ "1 2 3	1=down-left 2=down 3=down-right");
-	
+
 	}
-	
+
 	/**
 	 * Prints the Track selection request
 	 */
 	public void printTrackSelectionMessage() {
 		textTerminal.println("Please select a track:");
 	}
-	
+
 	/**
 	 * Prints the MoveList selection request
+	 * 
 	 * @param carID the cars symbol for which the move list is selected for
 	 */
 	public void printMoveListSelectionMessage(char carID) {
 		textTerminal.println("Please select a move list for car " + carID + ":");
 	}
-	
+
 	/**
 	 * Prints the WayPoint selection request
+	 * 
 	 * @param carID the cars symbol for which the waypoint list is selected for
 	 */
 	public void printWayPointListSelectionMessage(char carID) {
 		textTerminal.println("Please select a waypoint list to follow for car " + carID + ":");
 	}
-	
+
 }
