7d40c90b817c9e5b1292732365d83a955727f55b
meierr02 <meierr02@students.zhaw.ch>
2021-03-21 21:52:24 +0100

    added comparison list

--- src/test/java/ch/zhaw/pm2/racetrack/PathFollowerMoveStrategyTest.java
+++ src/test/java/ch/zhaw/pm2/racetrack/PathFollowerMoveStrategyTest.java
@@ -2,6 +2,9 @@ package ch.zhaw.pm2.racetrack;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,8 @@ import ch.zhaw.pm2.racetrack.strategy.*;
 public class PathFollowerMoveStrategyTest {
 	private Game _testGame;
 	private MoveStrategy _testStrategy;
+	private Scanner followerReader;
+	private List<PositionVector> followerPoints;
 	
 	@BeforeEach
 	private void Setup() {
@@ -41,9 +46,44 @@ public class PathFollowerMoveStrategyTest {
 		_testGame = new Game(carCount, track);
 	}
 	
-	@Test
-	public void testPositionEquivalence() {
-		
+	private void setupFollowerCompareList(File followerPointList){
+		try {
+			followerReader = new Scanner(followerPointList);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(followerReader.hasNextLine()) {
+			String nextLine = followerReader.nextLine();
+			String[] parts = nextLine.split("");
+			int x1 = 0;
+			int x2 = 0;
+			int y1 = 0;
+			int y2 = 0;
+			
+			try {
+				x1 = Integer.parseInt(parts[3])*10;
+				x2 = Integer.parseInt(parts[4]);
+			}catch (NumberFormatException e) {
+				x1 = x1/10;
+				x2 = 0;
+			}
+			
+			try {
+				y1 = Integer.parseInt(parts[9])*10;
+				y2 = Integer.parseInt(parts[10]);
+			}catch (NumberFormatException e) {
+				y1 = y1 /10;
+				y2 = 0;
+			}
+			int x = x1 + x2;
+		    int y = x1 + y2;
+			
+			followerPoints.add(new PositionVector(x,y));
+		}
+		followerReader.close();
 	}
+	
+	@Test
+	public void testPositionEquivalence() {}
 
 }
